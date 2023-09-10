e82be0b265ce791a619245bea8a4361ed20935fc
Kostia Nahula <kostyannagula@gmail.com>
2023-09-10 20:25:42 +0300

    added validation to test form

--- src/components/TestForm/TestForm.tsx
+++ src/components/TestForm/TestForm.tsx
@@ -3,6 +3,7 @@ import { useFormik, FormikProvider, FieldArray, Field } from 'formik';
 import * as Yup from 'yup';
 import { FormData } from '@/components/TestForm/models';
 import { newCategory, newQuestion } from '@/components/TestForm/consts';
+import { checkError } from '@/helpers/errorMessage';
 
 export const TestForm = () => {
 
@@ -11,7 +12,17 @@ export const TestForm = () => {
     categories: Yup.array()
       .of(
         Yup.object().shape({
-          name: Yup.string().required().min(5)
+          name: Yup.string()
+            .required()
+            .min(5),
+          questions: Yup.array()
+            .of(
+              Yup.object().shape({
+                title: Yup.string().required().min(5),
+                description: Yup.string().required().min(5),
+                answer: Yup.string().required().min(5),
+              }),
+            )
         })
       )
   });
@@ -35,7 +46,8 @@ export const TestForm = () => {
 
   const onAddCategory = (e: React.FormEvent<EventTarget>) => {
     e.preventDefault();
-    setFieldValue('categories', [...values.categories, newCategory]);
+    console.log(newCategory, 'new category');
+    setFieldValue('categories', [...values.categories, {...newCategory}]);
   }
 
   const onAddQuestion = (e: React.FormEvent<EventTarget>, index: number) => {
@@ -44,6 +56,9 @@ export const TestForm = () => {
     categories[index].questions = [...categories[index].questions, newQuestion];
     setFieldValue('categories', categories);
   }
+
+  console.log(errors, 'errors');
+  // console.log(values, 'values');
 
   return (
     <div>
@@ -80,7 +95,7 @@ export const TestForm = () => {
                   <div>
                     <label
                       htmlFor="name"
-                      className={`block text-sm font-medium leading-6 text-gray-900`}
+                      className={`block text-sm font-medium leading-6 ${checkError(['categories', 'name'], `categories[${i}].name`, errors) ? 'text-red-700 dark:text-red-500' : 'text-gray-900'}`}
                     >
                       <div className="flex justify-between">
                         <>Category</>
@@ -95,8 +110,93 @@ export const TestForm = () => {
                         name={`categories.${i}.name`}
                         onChange={handleChange}
                         value={values.categories[i].name}
-                        className={`block w-full rounded-md 'border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                        className={`block w-full rounded-md ${checkError(['categories', 'name'], `categories[${i}].name`, errors) ? 'bg-red-50 border border-red-500' : 'border-0'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
                       />
+                      {checkError(['categories', 'name'], `categories[${i}].name`, errors) 
+                        && <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                          {checkError(['categories', 'name'], `categories[${i}].name`, errors)}
+                        </p>
+                      }
+                      {category.questions.map((_, qi) => {
+                        return (
+                          <FieldArray
+                            key={qi}
+                            name={`categories.${i}.questions`}
+                            render={(prop) => (
+                              <div className="mt-2">
+                                <div>
+                                  <label
+                                    htmlFor="title"
+                                    className={`block text-sm font-medium leading-6 ${checkError(['categories', 'questions', 'title'], `categories[${i}].questions[${qi}].title`, errors) ? 'text-red-700 dark:text-red-500' : 'text-gray-900'}`}
+                                  >
+                                    <div className="flex justify-between">
+                                      <>Title</>
+                                      <button
+                                        className="mt-2 placeholder:font-semibold text-indigo-600 hover:text-indigo-500"
+                                        onClick={() => prop.remove(qi)}
+                                      > - delete question</button>
+                                    </div>
+                                  </label>
+                                  <Field
+                                    name={`categories.${i}.questions.${qi}.title`}
+                                    onChange={handleChange}
+                                    value={values.categories[i].questions[qi].title}
+                                    className={`block w-full rounded-md ${checkError(['categories', 'questions', 'title'], `categories[${i}].questions[${qi}].title`, errors) ? 'bg-red-50 border border-red-500' : 'border-0'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                                  />
+                                  {checkError(['categories', 'questions', 'title'], `categories[${i}].questions[${qi}].title`, errors) 
+                                    && <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                                      {checkError(['categories', 'questions', 'title'], `categories[${i}].questions[${qi}].title`, errors)}
+                                    </p>
+                                  }
+                                </div>
+                                <div className="mt-2">
+                                  <label
+                                    htmlFor="body"
+                                    className={`block text-sm font-medium leading-6 ${checkError(['categories', 'questions', 'description'], `categories[${i}].questions[${qi}].description`, errors) ? 'text-red-700 dark:text-red-500' : 'text-gray-900'}`}
+                                  >
+                                    Description
+                                  </label>
+                                  <Field
+                                    name={`categories.${i}.questions.${qi}.description`}
+                                    component="textarea"
+                                    rows="3"
+                                    onChange={handleChange}
+                                    value={values.categories[i].questions[qi].description}
+                                    className={`block w-full rounded-md ${checkError(['categories', 'questions', 'description'], `categories[${i}].questions[${qi}].description`, errors) ? 'bg-red-50 border border-red-500' : 'border-0'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                                  />
+                                  {checkError(['categories', 'questions', 'description'], `categories[${i}].questions[${qi}].description`, errors) 
+                                    && <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                                      {checkError(['categories', 'questions', 'description'], `categories[${i}].questions[${qi}].description`, errors)}
+                                    </p>
+                                  }
+                                </div>
+                                <div className="mt-2 mb-5">
+                                  <label
+                                    htmlFor="body"
+                                    className={`block text-sm font-medium leading-6 ${checkError(['categories', 'questions', 'answer'], `categories[${i}].questions[${qi}].answer`, errors) ? 'text-red-700 dark:text-red-500' : 'text-gray-900'}`}
+                                  >
+                                    Answer
+                                  </label>
+                                  <Field
+                                    name={`categories.${i}.questions.${qi}.answer`}
+                                    component="textarea"
+                                    rows="3"
+                                    onChange={handleChange}
+                                    value={values.categories[i].questions[qi].answer}
+                                    className={`block w-full rounded-md ${checkError(['categories', 'questions', 'answer'], `categories[${i}].questions[${qi}].answer`, errors) ? 'bg-red-50 border border-red-500' : 'border-0'} p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                                  />
+                                  {checkError(['categories', 'questions', 'answer'], `categories[${i}].questions[${qi}].answer`, errors) 
+                                    && <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                                      {checkError(['categories', 'questions', 'answer'], `categories[${i}].questions[${qi}].answer`, errors)}
+                                    </p>
+                                  }
+                                </div>
+                                <hr/>
+                              </div>
+                            )}
+                          />
+                        );
+                      })}
                       <div>
                         <button
                           type="button"
@@ -104,73 +204,6 @@ export const TestForm = () => {
                           onClick={(e) => onAddQuestion(e, i)}
                         > + add question</button>
                       </div>
-                      {category.questions.map((question, qi) => {
-                        return (
-                          <FieldArray
-                            key={qi}
-                            name={`categories.${i}.questions`}
-                            render={(prop) => {
-                              return (
-                                <div className="mt-2">
-                                  <div>
-                                    <label
-                                      htmlFor="title"
-                                      className={`block text-sm font-medium leading-6 text-gray-900`}
-                                    >
-                                      <div className="flex justify-between">
-                                        <>Title</>
-                                        <button
-                                          className="mt-2 placeholder:font-semibold text-indigo-600 hover:text-indigo-500"
-                                          onClick={() => prop.remove(i)}
-                                        > - delete question</button>
-                                      </div>
-                                    </label>
-                                    <Field
-                                      name={`categories.${i}.questions.${qi}.title`}
-                                      onChange={handleChange}
-                                      value={values.categories[i].questions[qi].title}
-                                      className={`block w-full rounded-md 'border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-                                    />
-                                  </div>
-                                  <div className="mt-2">
-                                    <label
-                                      htmlFor="body"
-                                      className={`block text-sm font-medium leading-6 text-gray-900`}
-                                    >
-                                      Description
-                                    </label>
-                                    <Field
-                                      name={`categories.${i}.questions.${qi}.description`}
-                                      component="textarea"
-                                      rows="3"
-                                      onChange={handleChange}
-                                      value={values.categories[i].questions[qi].description}
-                                      className={`block w-full rounded-md 'border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-                                    />
-                                  </div>
-                                  <div className="mt-2 mb-5">
-                                    <label
-                                      htmlFor="body"
-                                      className={`block text-sm font-medium leading-6 text-gray-900`}
-                                    >
-                                      Answer
-                                    </label>
-                                    <Field
-                                      name={`categories.${i}.questions.${qi}.answer`}
-                                      component="textarea"
-                                      rows="3"
-                                      onChange={handleChange}
-                                      value={values.categories[i].questions[qi].answer}
-                                      className={`block w-full rounded-md 'border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
-                                    />
-                                  </div>
-                                  <hr/>
-                                </div>
-                              );
-                            }}
-                          />
-                        );
-                      })}
                     </div>
                   </div>
                 </div>
@@ -189,7 +222,7 @@ export const TestForm = () => {
           type="submit"
           className="flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Sign in
+          Submit
         </button>
       </form>
     </div>
---
+++ src/helpers/errorMessage.ts
@@ -0,0 +1,40 @@
+export const checkError = (
+  fields: string[],
+  messageField: string,
+  errors: Record<string, any>,
+): string | undefined => {
+  const [arg1, arg2, arg3] = fields;
+  const replaceTo = arg3 || arg2;
+
+  if (!Object.entries(errors).length) {
+    return undefined;
+  }
+
+  if (arg1 && arg2 && arg3) {
+    const list = errors[arg1];
+    
+    if (list.length) {
+      const nestedList = list.find((item: Record<string, any>) => 
+        item[arg2]
+      );
+      if (nestedList && nestedList[arg2] && nestedList[arg2].length) {
+        const message = nestedList[arg2]
+          .find((item: Record<string, string>) => 
+            item && item[arg3] && item[arg3].includes(messageField)
+          );
+        return message && message[arg3]?.replace(messageField, replaceTo);
+      }
+    }
+
+  } else if (arg1 && arg2) {
+    const list = errors[arg1];
+
+    if (list?.length) {
+      const message = list.find(
+        (item: Record<string, string>) => item && item[arg2] && item[arg2].includes(messageField)
+      );
+
+      return message && message[arg2]?.replace(messageField, replaceTo);
+    }
+  }
+};
